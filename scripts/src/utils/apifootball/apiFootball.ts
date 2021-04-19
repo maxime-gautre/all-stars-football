c375ae94ecae8b5f3820812492eb1eeaa103e5db
Maxime Gautré <maxime.gautre@tinyclues.com>
2021-04-19 14:11:52 +0200

    Import axios module from github
    The latest versions of the axios module has not yet been published on the deno land registry.
    The fix to handle query paramaters is already implemented but not published. To use this fix, we import the axios module from github directly.

--- scripts/src/utils/apifootball/apiFootball.ts
+++ scripts/src/utils/apifootball/apiFootball.ts
@@ -8,17 +8,17 @@ export async function fetchData<T>(
   path: string,
   params: { [key: string]: string | number | boolean },
 ): Promise<FootballApiResponse<T>> {
-  const season = "2020";
-  const queryParameters = new URLSearchParams({
-    ...params,
-    season,
-  });
+  const season = 2020;
   const config = {
-    url: `https://v3.football.api-sports.io/${path}?${queryParameters}`,
+    url: `https://v3.football.api-sports.io/${path}`,
     method: "get" as const,
     headers: {
       "x-apisports-key": ApiSportsKey,
     },
+    params: {
+      ...params,
+      season
+    }
   };
   const res = await axiod.request(config);
   const data = (res.data as unknown) as FootballApiResponse<T>;
