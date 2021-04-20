599176998b03c4ea67bdc35115318998b997841e
Maxime Gautré <maxime.gautre@tinyclues.com>
2021-04-20 07:58:03 +0200

    Use new import for axiod as new release has been published

--- backend/deps.ts
+++ backend/deps.ts
@@ -1,6 +1,6 @@
 export * as log from "https://deno.land/std@0.93.0/log/mod.ts";
 export { Bson, MongoClient } from "https://deno.land/x/mongo@v0.22.0/mod.ts";
 export { Collection } from "https://deno.land/x/mongo@v0.22.0/src/collection/mod.ts";
-import axiod from "https://raw.githubusercontent.com/roonie007/axiod/master/mod.ts";
+import axiod from "https://deno.land/x/axiod/mod.ts";
 export { axiod };
 export { Application, Context, Router } from "https://deno.land/x/oak/mod.ts";
