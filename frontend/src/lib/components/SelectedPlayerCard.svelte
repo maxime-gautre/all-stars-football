bd80441873f698c3d8ea2f849cc2bf7e30116a2b
Maxime Gautré <maxime.gautre@tinyclues.com>
2021-04-30 18:12:18 +0200

    Improve design (#15)
    - Adds carbon-svelte-icons
    - Improves index and players page design

---
+++ frontend/src/lib/components/SelectedPlayerCard.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+  import type { SelectedPlayer } from '../stores/playerStore.ts';
+  import { Close16 } from 'carbon-icons-svelte';
+
+  export let player: SelectedPlayer;
+  export let onClose;
+</script>
+
+<div class="mini-player-card">
+  <span class="close-button" on:click={onClose}>
+    <Close16 />
+  </span>
+  <div class="player-images">
+    <img src={player.teamLogo} alt="Team's player logo" class="team-logo" />
+    <img src={player.photo} alt="{player.name}'s picture" class="player-photo" />
+  </div>
+  <div class="player-panel">
+    <h6>{player.name}</h6>
+  </div>
+</div>
+
+<style>
+  .mini-player-card {
+    position: relative;
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+    text-align: center;
+    width: 100px;
+    background-color: #eee;
+  }
+
+  .close-button {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    color: #333;
+  }
+
+  .player-images {
+    background-color: #fff;
+  }
+
+  .team-logo {
+    position: absolute;
+    right: 5px;
+    width: 20px;
+    top: 5px;
+  }
+
+  .player-photo {
+    border-radius: 5px;
+    height: 45px;
+  }
+
+  .player-panel {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 30px;
+  }
+</style>
---
+++ frontend/src/lib/components/VoteButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { Button } from 'carbon-components-svelte';
+  export let kind;
+  export let disabled;
+</script>
+
+<Button
+  style="width: 100%; border-radius: 5px; text-transform: uppercase; display: block; text-align: center; padding: 0;"
+  {kind}
+  {disabled}
+  on:click
+>
+  <slot />
+</Button>
